bb11ba44d7b3338e1b7aab51865e2d07ad26e0a5
felipe122-cpu <felipe122pro@gmail.com>
2026-05-26 13:48:41 -0500

    Proyecto

--- main.py
+++ main.py
@@ -9,11 +9,20 @@ def mensaje():
 
 @app.get("/clientes")
 def clientes():
-    return {
+    return [
+        {
         "id": 1,
         "nombre": "Chanty",
         "apellido": "Beltran",
         "correo": "ChantyBeltran@gmail.com",
         "telefono": "3214567890",
         "contraseña": "123456789"
-    }
+    },
+    {
+     "id": 2,
+        "nombre": "Eddy",
+        "apellido": "Suarez",
+        "correo": "EddySuarez@gmail.com",
+        "telefono": "3214567891",
+        "contraseña": "987654321"   
+    }]
