b723ab1de11b1007448f1e51cbd5a4a0ef0a284a
felipe122-cpu <felipe122pro@gmail.com>
2026-07-02 22:13:15 -0500

    modulo

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from modelos.cliente import cliente, ClearCliente
 
 app = FastAPI()
 
 lista_clientes:list[cliente] = []
 
-class cliente(BaseModel):
-    id: int
-    nombre: str
-    apellido:str
-    email: str
-    telefono: int
-    contraseña: str
 
-@app.get("/")
-def mensaje():
-    return {"mensaje": "Este es el proyecto de clientes a desarrollar"}
-
-
-@app.get("/clientes")
+@app.get("/clientes", response_model=list[cliente])
 def clientes():
     return lista_clientes
 
-@app.get("/clientes/{idClientes}")
+@app.get("/clientes/{idClientes}", response_model=(cliente))
 def clientes(cliente_id: int):
     for i, obj_cliente in enumerate (lista_clientes):
         if obj_cliente.get("id") == cliente_id:
-            return obj_cliente
+             return obj_cliente
 
-@app.post("/clientes")
-def crear_cliente(datos_cliente: cliente):
-    lista_clientes.append(datos_cliente)
-    return datos_cliente
+@app.post("/clientes", response_model=(cliente))
+def crear_cliente(datos_cliente: ClearCliente):
+    Validar_cliente = cliente.model_validate(datos_cliente.model_dump())
+    lista_clientes.append(Validar_cliente)
+    return Validar_cliente
