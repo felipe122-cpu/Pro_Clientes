145ad022bd2fdb82320ee76df438054205233600
felipe122-cpu <felipe122pro@gmail.com>
2026-07-06 22:34:33 -0500

    agregar listas en grobal

---
+++ app/listas.py
@@ -0,0 +1,7 @@
+from .modelos.cliente import cliente
+from .modelos.factura import factura
+from .modelos.transaccion import Transaccion
+
+lista_cliente: list[Cliente] = []
+lista_factura: list[Factura] = []
+lista_transaccion: list[Transaccion] = []
